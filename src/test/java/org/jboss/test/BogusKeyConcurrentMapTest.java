ff026fafcce4b39f38cb09d25b53b8182d6a8002
Jason T. Greene <jason.greene@redhat.com>
2013-11-20 14:11:46 -0600

    Add a test to demonstrate broken equals

--- src/test/java/org/jboss/test/BogusKeyConcurrentMapTest.java
+++ src/test/java/org/jboss/test/BogusKeyConcurrentMapTest.java
@@ -10,18 +10,19 @@ import org.junit.Test;
 public class BogusKeyConcurrentMapTest {
     public static class Bogus {
         private Integer value;
-        public static boolean breakme = false;
+        public static boolean breakhash = false;
+        public static boolean breakequals = false;
 
         public Bogus(Integer value) {
             this.value = value;
         }
 
         public int hashCode() {
-            return breakme ? -1 : value.hashCode();
+            return breakhash ? -1 : value.hashCode();
         }
 
         public boolean equals(Object o) {
-            if (!(o instanceof Bogus)) {
+            if (breakequals || !(o instanceof Bogus)) {
                 return false;
             }
 
@@ -32,7 +33,7 @@ public class BogusKeyConcurrentMapTest {
 
 
     @Test
-    public void testBogusRemove() {
+    public void testBogusHashOnly() {
         BogusKeyConcurrentMap<Bogus, Object> map = new BogusKeyConcurrentMap<Bogus, Object>();
 
         Bogus save = null;
@@ -45,14 +46,42 @@ public class BogusKeyConcurrentMapTest {
         int hash = save.hashCode();
 
         Assert.assertNotNull(map.get(save));
-        Bogus.breakme = true;
+        Bogus.breakhash = true;
+
+        Assert.assertNull(map.remove(save));
+        Assert.assertEquals(1000, map.size());
+        Assert.assertNotNull(map.removeDeadKey(hash, new Bogus(737)));
+        Assert.assertEquals(999, map.size());
+
+        Bogus.breakhash = false;
+
+        Assert.assertNull(map.get(save));
+    }
+
+    @Test
+    public void testBogusHashAndEquals() {
+        BogusKeyConcurrentMap<Bogus, Object> map = new BogusKeyConcurrentMap<Bogus, Object>();
+
+        Bogus save = null;
+        for (int i = 0; i < 1000; i++) {
+            Bogus key = new Bogus(i);
+            map.put(key, new Object());
+            if (i == 737) save = key;
+        }
+
+        int hash = save.hashCode();
+
+        Assert.assertNotNull(map.get(save));
+        Bogus.breakhash = true;
+        Bogus.breakequals = true;
 
         Assert.assertNull(map.remove(save));
         Assert.assertEquals(1000, map.size());
         Assert.assertNotNull(map.removeDeadKey(hash, save));
         Assert.assertEquals(999, map.size());
 
-        Bogus.breakme = false;
+        Bogus.breakhash = false;
+        Bogus.breakequals = false;
 
         Assert.assertNull(map.get(save));
     }
